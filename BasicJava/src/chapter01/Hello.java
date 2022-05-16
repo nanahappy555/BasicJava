b5f12fedae79811bcf001f0d7c87ec9ecc5eca70
nanahappy555 <nanahappy555@gmail.com>
2022-05-16 16:56:46 +0900

    2022-04-06

---
+++ BasicJava/src/chapter01/Hello.java
@@ -0,0 +1,27 @@
+package chapter01;
+/**
+ * 도큐먼트 or 도큐먼트 주석 : API 문서 만들 때 사용
+ * @author LHR
+ * @since 2022
+ */
+public class Hello {
+	public static void main(String[] args) {
+//		Hello
+		// 한줄 주석 
+		/*
+		 * 
+		 * 여러줄 주석
+		 * 
+		 */
+		/**
+		 * 도큐먼트 or 도큐먼트 주석 : API 문서 만들 때 사용
+		 */
+		/*여러
+		 * 줄
+		 * 주석
+		 * 입니당
+		 */
+		System.out.println("Hello, Java");
+	}
+
+}
