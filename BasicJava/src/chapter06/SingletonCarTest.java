1299a6abb78ff030232498963209fe9839d2b3cc
RIM <80573832+nanahappy555@users.noreply.github.com>
2022-06-27 09:08:36 +0900

    Add files via upload

---
+++ BasicJava/src/chapter06/SingletonCarTest.java
@@ -0,0 +1,11 @@
+
+public class SingletonCarTest {
+	public static void main(String[] args) {
+		SingletonCar car1 = SingletonCar.getInstance();
+		SingletonCar car2 = SingletonCar.getInstance();
+		
+		System.out.println(car1);
+		System.out.println(car2);
+		//같은 주소값을 가짐
+	}
+}
